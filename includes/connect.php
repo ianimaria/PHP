ffe411de2c36f1218a91bc4c26bbbf79ff28fb13
Maria Iani <56823621+ianimaria@users.noreply.github.com>
2020-11-18 11:50:32 +0200

    Add files via upload

--- includes/connect.php
+++ includes/connect.php
@@ -1,14 +1,14 @@
 <? php 
 
+$host_name = "eu-cdbr-west-03.cleardb.net";
 $user_name = "b7d90ae59c07c3";
 $password = "ca987d22";
 $database = "iroom";
-$host_name = "eu-cdbr-west-03.cleardb.net";
 
 $conn = mysqli_connect($host_name, $user_name, $password, $database);
 if (!$conn)
 {
-    die("Connection failed: ". mysqli_connect_error());
+    die("Connection failed: ".mysqli_connect_error());
 }
 
 ?>
