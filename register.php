ffb1c0556d5f815e55339fa013e72c44746f4ae9
Maria Iani <56823621+ianimaria@users.noreply.github.com>
2020-11-18 13:08:36 +0200

    Add files via upload

--- register.php
+++ register.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" type="text/css" href="register.css">
 </head>
 <body>
-<form action="includes/registered.php" method="post">
+<form method="post">
         <p><center><input class="w3-input-group w3-input " type="text" name="email" placeholder="Email" style="width:60%" required><br>
         </p>
         <p><input class="w3-input-group w3-input" type="password" name="password" placeholder="Password" style="width:60%" required><br>		
@@ -20,7 +20,7 @@
         </p>
         </center>
       
-      <input type="submit" name="submit" value="Submit" class="w3-btn w3-section w3-center"></input>
+      <input type="submit" action="includes/registered.php" name="submit" value="Submit" class="w3-btn w3-section w3-center"></input>
 
 </form>
 </body>
