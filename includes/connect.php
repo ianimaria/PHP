abe16d5d7078da4892ad1d0989b908994f5e50f8
Maria Iani <56823621+ianimaria@users.noreply.github.com>
2020-11-18 21:25:16 +0200

    Add files via upload

--- includes/connect.php
+++ includes/connect.php
@@ -1,9 +1,9 @@
 <?php 
 
-$host_name = "eu-cdbr-west-03.cleardb.net";
-$user_name = "b7d90ae59c07c3";
-$password = "ca987d22";
-$database = "heroku_417556086059fa1";
+$host_name = "localhost";
+$user_name = "root";
+$password = "";
+$database = "iroom";
 
 $conn = mysqli_connect($host_name, $user_name, $password, $database);
 if (!$conn) {
--- includes/registered.php
+++ includes/registered.php
@@ -1,28 +1,37 @@
 <?php   
+$host_name = "localhost";
+$user_name = "root";
+$password = "";
+$database = "iroom";
+
+$conn = mysqli_connect($host_name, $user_name, $password, $database);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  else
+   {echo "Merge!!!!!!";}
+
     if(isset($_POST["submit"]))
     {
-        require 'connect.php';
-        $email=$_POST['email'];
-        $pass=$_POST['password'];
-        $first_name=$_POST['first_name'];
-        $last_name=$_POST['last_name'];
-        $phone=$_POST['phone'];
         
-        $sql = "INSERT INTO guest(email, pass, first_name, last_name, phone) VALUES (?, ?, ?, ?, ?);";
-        $statement=mysqli_stmt_init($conn);
-        if (!mysqli_stmt_prepare($statement, $sql)){
-            header("Location; ../register.php?error=sqlerror");
-            exit();
-        }
-        $checkpass = password_hash($password, PASSWORD_DEFAULT);
-            mysqli_stmt_bind_param($statement, "sssss", $email, $pass, $first_name, $last_name, $phone);
-            mysqli_stmt_execute($statement);
-            mysqli_stmt_close($statement);
+        $email=$_POST["email"];
+        $pass=$_POST["password"];
+        $first_name=$_POST["first_name"];
+        $last_name=$_POST["last_name"];
+        $phone=$_POST["phone"];
+        
+        $sql = "INSERT INTO guest(EMAIL, PASSWORD, FIRST_NAME, LAST_NAME, PHONE) VALUES ('$email', '$pass', '$first_name', '$last_name', '$phone');";
+
+        if (mysqli_query($conn, $sql)) {
             echo '<script language="javascript">';
             echo 'alert("You are successfully registered!")';
             echo '</script>';
-            exit();
-                        
-                    }
+        } else {
+            echo '<script language="javascript">';
+            echo 'alert("An error occured")';
+            echo '</script>';
+        }
 
-?>
+        mysqli_close($conn);}
+        ?>
+      
