73f698b26249c43c78655105ed1db70337c133e0
Maria Iani <56823621+ianimaria@users.noreply.github.com>
2020-11-18 13:57:54 +0200

    Add files via upload

--- includes/connect.php
+++ includes/connect.php
@@ -6,8 +6,18 @@ $password = "ca987d22";
 $database = "iroom";
 
 $conn = mysqli_connect($host_name, $user_name, $password, $database);
-if (!$conn)
-{
-    die("Connection failed: ".mysqli_connect_error());
-}
-?>
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  
+  $sql = "INSERT INTO guest (email, password, first_name, last_name, phone)
+  VALUES ('maria@yahoo.com', 'blabla', 'maria', 'iani', '23432432')";
+  
+  if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+  mysqli_close($conn);
+  ?>
