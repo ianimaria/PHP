2d17e0566204dd50b3f50b22b53ebf76f9085f47
Maria Iani <56823621+ianimaria@users.noreply.github.com>
2020-11-18 11:52:38 +0200

    Add files via upload

--- includes/registered.php
+++ includes/registered.php
@@ -1,48 +1,65 @@
 <?php
-    include 'connect.php';
+   
     if(isset($_POST["submit"]))
     {
-        echo "It works";}
+        require 'connect.php';
+        $email=$_POST['email'];
+        $password=$_POST['password'];
+        $first_name=$_POST['first_name'];
+        $last_name=$_POST['last_name'];
+        $phone=$_POST['phone'];
+
+        $sql = "SELECT guest_id FROM guest WHERE email=?;";
+        $statement=mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($statement, $sql)){
+            header("Location; ../register.php?error=sqlerror");
+            exit();
+        }
         else
-        { "it does not work";}
-    //     $email=$_POST['email'];
-    //     $password=$_POST['password'];
-    //     $first_name=$_POST['first_name'];
-    //     $last_name=$_POST['last_name'];
-    //     $phone=$_POST['phone'];
+        {
+            mysqli_stmt_bind_param($statement, "s", "email");
+            mysqli_stmt_execute($statement);
+            mysqli_stmt_store_result($statement);
 
-    //     $check ="SELECT email FROM guest WHERE email ='$email";
-    //     $temp=$mysqli->query($check);
-    //     if ($temp->num_rows==0)
-    //     {
-    //         if ($mysqli->error ==0)
-    //         {
-    //             $sql = "INSERT INTO guest VALUES (default, '$email', '$password', '$first_name', '$last_name', '$phone')";
-    //             $result = $mysqli->query($sql);
-    //         }
+            $result = mysqli_stmt_num_rows($statement);
+            $id = uniqid($email);
 
-    //         if ($result)
-    //         {
-    //             echo '<script language="javascript">';
-    //             echo 'alert("You are successfully registered!")';
-    //             echo '</script>';
-    //         }
-    //         else
-    //         {
-    //             echo '<script language="javascript">';
-    //             echo 'alert("An error occurred")';
-    //             echo '</script>';
-    //         }
-    //     }
-    // }
-    // else
-    // {
-    //     echo '<script language="javascript">';
-    //     echo 'alert("Email already registered, register with another email")';
-    //     echo '</script>';
-    // }
-?>
+            if ($result > 0)
+            {
+                echo '<script language="javascript">';
+                echo 'alert("There is already an account with this email")';
+                echo '</script>';
+                exit();
+            }
+            else
+            {
+                $checkpass = password_hash($password, PASSWORD_DEFAULT);
+                $sql = "INSERT INTO guest(email, password, first_name, last_name, phone) VALUES (?, ?, ?, ?, ?);";
+                $statement = mysqli_stmt_init($conn);
 
-<?php
-    include 'connect.php';
+                if (!mysqli_stmt_init($conn))
+                    {header("Location: ../signup.php?error=sqlerror");
+                    exit(); }
+                else
+                    {
+                        mysqli_stmt_bind_param($statement, "sssss", $email, $checkpass, $first_name, $last_name, $phone);
+                        mysqli_stmt_execute($statement);
+                        mysqli_stmt_store_result($statement);
+                        echo '<script language="javascript">';
+                        echo 'alert("You are successfully registered!")';
+                        echo '</script>';
+                        exit();
+                        
+                    }
+            }
+        }
+        mysqli_stmt_close($statement);
+        mysqli_close($conn);
+    }
+    else
+    {
+        header("Location: ../index.php");
+        exit();
+    }
+
 ?>
